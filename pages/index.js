7e803fd5d2b53708eb01168a701ace629c0037f5
Felipe Assis Andrade <fe.a.andrade@hotmail.com>
2026-07-29 00:03:33 +0000

    alterando o texto da pagina

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Você é incrível e capaz de coisas que nem sabe ainda!</h1>
+    return <h1>Sou loucamente apaixonado por você, my lady! E se você também é por mim, me manda um beijinho <span>&#128536</span></h1>
 }
 
 export default Home;
