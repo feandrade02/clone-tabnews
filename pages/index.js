b0998bf04ec24a095f36417fb08166f77e339a4c
Felipe Assis Andrade <fe.a.andrade@hotmail.com>
2026-08-13 02:03:08 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-    return <h1>Sou loucamente apaixonado por você, my lady! E se você também é por mim, me manda um beijinho <span>😘​</span></h1>
+  return (
+    <h1>
+      Sou loucamente apaixonado por você, my lady! E se você também é por mim,
+      me manda um beijinho <span>😘​</span>
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
